ff0ebbcebac5bb909e5c469d2602b0b660c50681
Jeremie Beliveau-Lefebvre <jeremie.beliveau-lefebvre@usherbrooke.ca>
2019-11-15 20:47:25 -0500

    Finalisation

--- TP3/prog/non_lineaire_classification.py
+++ TP3/prog/non_lineaire_classification.py
@@ -24,9 +24,10 @@ def analyse_erreur(err_train, err_test):
     Fonction qui affiche un WARNING lorsqu'il y a apparence de sur ou de sous
     apprentissage
     """
-    #AJOUTER CODE ICI
-    print('WARNING: overfitting')
-    print('WARNING: underfitting')
+    if err_train < 5. and err_test > 30.:       
+        print('WARNING: overfitting')             
+    if err_test >= 30. and err_train >= 30.:         
+        print('WARNING: underfitting')
 
 def main():
 
